c3df5adcd3946de50c8704449804feac0073fb85
ssteinme <shannon.steinmetz@ucdenver.edu>
2017-11-14 15:23:04 -0700

    Initial Baseline

---
+++ core/src/core/math/CRT.java
@@ -0,0 +1,24 @@
+package core.math;
+
+/**
+ * Provides a computation of the chinese remainder theorem.
+ * @author Shannon
+ */
+public class CRT {
+  
+  /**
+   * Compute the CRT provided the moduli and remainders.
+   * @return The value of (x).
+   */
+  public long CRT(long[] moduli, long[] remainders) {
+    long M = 0;
+    for(int i=0;i<moduli.length;i++) M += moduli[i];
+    
+    for(int i=0;i<moduli.length;i++) {
+      
+      } 
+    
+    
+    }
+  
+  }
